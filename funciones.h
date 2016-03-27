4266ccdb7d0439fc13226e30e1c500ff795fea67
Albert Villarroel <zonecero777@gmail.com>
2016-03-27 17:18:53 -0300

    Desarrollo
    Desarrollo del programa, Main.c y Funciones.h

--- funciones.h
+++ funciones.h
@@ -1,6 +1,58 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
+//Prototipos de las Funciones
+double Suma(double x, double y);
+double Resta(double x, double y);
+double Multiplicacion(double x, double y);
+double Division(double x, double y);
+double Factoreo(double x);
 
+//Desarrollo de las Funciones
+double Suma(double x, double y)
+{
+    double suma;
 
+    suma = x + y;
+
+    return(suma);
+}
+
+double Resta(double x, double y)
+{
+    double resta;
+
+    resta = x - y;
+
+    return(resta);
+}
+
+double Multiplicacion(double x, double y)
+{
+    double producto;
+
+    producto = x * y;
+
+    return(producto);
+}
+
+double Division(double x, double y)
+{
+    double division;
+
+    division = x / y;
+
+    return(division);
+}
+
+double Factoreo(double x)
+{
+    double resp;
+    if(x == 1)
+        return(1);
+
+    resp = x*Factoreo(x-1);
+
+    return(resp);
+}
 
 #endif // FUNCIONES_H_INCLUDED
--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 #include "funciones.h"
 
 int main()
 {
     char seguir='s';
-    int opcion=0;
+    double x = 0, y = 0, suma, resta, division, multiplicacion, factoreo;
+    int opcion = 0;
+    int band = 0, band1 = 0, band2 = 0;
 
     while(seguir=='s')
     {
-        printf("1- Ingresar 1er operando (A=x)\n");
-        printf("2- Ingresar 2do operando (B=y)\n");
+        printf("1- Ingresar 1er operando (A = %.0Lf)\n",x);
+        printf("2- Ingresar 2do operando (B = %.0Lf)\n",y);
         printf("3- Calcular la suma (A+B)\n");
         printf("4- Calcular la resta (A-B)\n");
         printf("5- Calcular la division (A/B)\n");
@@ -21,23 +24,136 @@ int main()
 
         scanf("%d",&opcion);
 
+        system("cls");
+
         switch(opcion)
         {
             case 1:
+                printf("Ingresar el primer numero : ");
+                scanf("%Lf", &x);
+                system("cls");
+                band = 1;
                 break;
             case 2:
+                printf("\nIngresar el segundo numero : ");
+                scanf("%Lf", &y);
+                system("cls");
+                band1 = 1;
+                if(y == 0)
+                {
+                    band2 = 1;
+                }
+                else
+                {
+                    band2 = 0;
+                }
                 break;
             case 3:
+                if(band == 1 && band1 ==1)
+                {
+                    suma = Suma(x,y);
+                    printf("\nLa suma es : %.0Lf \n",suma);
+                    system("pause");
+                    system("cls");
+                }
+                else
+                {
+                    printf("Ingrese primero Los numeros en la opcion 1 y 2 \n");
+                    system("pause");
+                    system("cls");
+                }
+                system("cls");
                 break;
             case 4:
+                if(band == 1 && band1 ==1)
+                {
+                    resta = Resta(x,y);
+                    printf("\nLa resta es : %.0Lf \n",resta);
+                    system("pause");
+                    system("cls");
+                }
+                else
+                {
+                    printf("Ingrese primero Los numeros en la opcion 1 y 2 \n");
+                    system("pause");
+                    system("cls");
+                }
+                system("cls");
                 break;
             case 5:
+                if(band2 == 0)
+                {
+                    division = Division(x,y);
+                    printf("La Division es : %.0Lf \n",division);
+                    system("pause");
+                    system("cls");
+                }
+                else
+                {
+                    printf("No se puede Dividir por 0 \n");
+                    system("pause");
+                    system("cls");
+                }
                 break;
             case 6:
+                if(band == 1 && band1 ==1)
+                {
+                    multiplicacion = Multiplicacion(x,y);
+                    printf("\nLa Multiplicacion es : %.0Lf \n",multiplicacion);
+                    system("pause");
+                    system("cls");
+                }
+                else
+                {
+                    printf("\nIngrese primero Los numeros en la opcion 1 y 2 \n");
+                    system("pause");
+                    system("cls");
+                }
                 break;
             case 7:
+                if(band == 1)
+                {
+                    factoreo = Factoreo(x);
+                    printf("\nEl numero %.0Lf su factorial es : %.0Lf \n",x ,factoreo);
+                    system("pause");
+                    system("cls");
+                }
+                else
+                {
+                    printf("Por favor ingrese 1 valor para poder hacer la Factorizacion \n");
+                    system("pause");
+                    system("cls");
+                }
                 break;
             case 8:
+                if(band == 1 && band1 && 1)
+                {
+                    suma = Suma(x,y);
+                    printf("\nLa suma es : %.0Lf \n",suma);
+
+                    resta = Resta(x,y);
+                    printf("\nLa resta es : %.0Lf \n",resta);
+
+                    if(band2 == 0)
+                    {
+                        division = Division(x,y);
+                        printf("\nLa Division es : %.0Lf \n",division);
+                    }
+                    else
+                    {
+                        printf("No se puede Dividir por 0 \n");
+                    }
+
+                    multiplicacion = Multiplicacion(x,y);
+                    printf("\nLa Multiplicacion es : %.0Lf \n",multiplicacion);
+
+                    factoreo = Factoreo(x);
+                    printf("\nEl numero %.0Lf su factorial es : %.0Lf \n",x ,factoreo);
+
+                    system("pause");
+                    system("cls");
+                }
+
                 break;
             case 9:
                 seguir = 'n';
